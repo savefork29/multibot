dacf2208ac901193212475a5b57bf5d9faaa3c22
❎❎itsneutral❎❎ <77259546+neutralarmy@users.noreply.github.com>
2021-04-08 17:55:24 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+pkg install figlet
+clear
+figlet install | lolcat
+clear
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL ðŸ”” " | lolcat
+xdg-open https://youtube.com/channel/UC6PezjDN1ofLSXn3onPxzpQ
+sleep 2
+clear
+pkg install nano
+clear
+pkg install python
+clear
+pip install requirements.txt
+clear
+pkg install libcurl
+clear
+gem install lolcat
+clear
+pip install bs4
+clear
+pip install mechanize
+clear
+pip install termocolor
+echo "selesai"
+clear
+python clickbotsNopassV7.py
